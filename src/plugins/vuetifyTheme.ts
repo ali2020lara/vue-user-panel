513d2894ad1d5c43eb6090ef0c9d11604df8dc10
Ali Ansari <ali969705@gmail.com>
2025-08-09 10:59:50 +0330

    feat: add dashborad items

--- src/plugins/vuetifyTheme.ts
+++ src/plugins/vuetifyTheme.ts
@@ -5,14 +5,18 @@ export const lightTheme: ThemeDefinition = {
     colors: {
         primary: '#3A6FF8',
         whiteBg: '#F8F8F8',
-        white: '#F8F8F8'
+        white: '#F8F8F8',
+        blue: '#083A50',
+        blueMain: '#F8F8F8',
     },
 }
 export const darkTheme: ThemeDefinition = {
     dark: true,
     colors: {
         primary: '#3A6FF8',
+        whiteBg: '#1A2029',
         white: '#F8F8F8',
-    whiteBg: '#1A2029'
+        blue: '#ffffff',
+        blueMain: '#14161D',
     }
 }
